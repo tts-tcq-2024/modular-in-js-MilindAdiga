1129e77b3c4501b3a389f8851fd98127d3e68ba0
MilindAdiga <124535083+MilindAdiga@users.noreply.github.com>
2024-07-16 20:01:28 +0530

    Create colorUtils.js

---
+++ app/colorUtils.js
@@ -0,0 +1,34 @@
+const MajorColorNames = require('./MajorColorNames');
+const MinorColorNames = require('./MinorColorNames');
+const ColorPair = require('./ColorPair');
+
+function getColorFromPairNumber(pairNumber) {
+    const minorSize = MajorColorNames.length;
+    const majorSize = MinorColorNames.length;
+
+    if (pairNumber < 1 || pairNumber > minorSize * majorSize) {
+        throw new Error(`Argument PairNumber: ${pairNumber} is outside the allowed range`);
+    }
+
+    const zeroBasedPairNumber = pairNumber - 1;
+    const majorIndex = parseInt(zeroBasedPairNumber / minorSize);
+    const minorIndex = parseInt(zeroBasedPairNumber % minorSize);
+
+    return new ColorPair(MajorColorNames[majorIndex], MinorColorNames[minorIndex]);
+}
+
+function getPairNumberFromColor(pair) {
+    const majorIndex = MajorColorNames.indexOf(pair.majorColor);
+    const minorIndex = MinorColorNames.indexOf(pair.minorColor);
+
+    if (majorIndex === -1 || minorIndex === -1) {
+        throw new Error(`Unknown Colors: ${pair.toString()}`);
+    }
+
+    return (majorIndex * MinorColorNames.length) + (minorIndex + 1);
+}
+
+module.exports = {
+    getColorFromPairNumber,
+    getPairNumberFromColor
+};
